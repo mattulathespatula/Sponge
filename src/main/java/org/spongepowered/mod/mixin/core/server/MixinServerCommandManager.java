72cf95d9d540f1a728ef7d572e37f181a8e46f87
Chris Sanders <zidane@outlook.com>
2015-06-02 00:38:27 -0500

    Move ServerConfigurationManager to Common
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/org/spongepowered/mod/mixin/core/server/MixinServerCommandManager.java
+++ src/main/java/org/spongepowered/mod/mixin/core/server/MixinServerCommandManager.java
@@ -37,7 +37,7 @@ import org.spongepowered.common.interfaces.IMixinServerCommandManager;
 import org.spongepowered.mod.command.ForgeMinecraftCommandWrapper;
 
 @NonnullByDefault
-@Mixin(value = ServerCommandManager.class, priority = 1111) // TODO: Shouldn't this take the priority from the config?
+@Mixin(value = ServerCommandManager.class)
 public abstract class MixinServerCommandManager extends CommandHandler implements IMixinServerCommandManager {
 
     @Override
